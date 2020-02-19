99209ebadccca645d4b15e178b9738ee8facc6b0
Pudintea <najzmitea@gmail.com>
2020-02-19 15:15:34 +0700

    upload databases yang siap di import di phpmyadmin

---
+++ public/db/phpmvc-19-02-2020.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Feb 2020 pada 09.14
+-- Versi server: 10.1.36-MariaDB
+-- Versi PHP: 5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpmvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nrp` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `jurusan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`) VALUES
+(1, 'Pudin Saepudin Ilham', '081381729540', 'najzmitea@hmail.com', 'Teknik Informatika'),
+(2, 'Najzmi Ilham', '083820436185', 'najzmiilham@gmail.com', 'Teknik Mesin'),
+(3, 'Nicolas', '085221389555', 'nicolas@gmail.com', 'Teknik Industri'),
+(4, 'kang Erik', '12345334', 'kangerik@email.com', 'Teknik Informatika'),
+(5, 'Musaamin', '1234323', 'musaamin@web.id', 'Teknik Industri'),
+(6, 'Musaamin', '1234323', 'musaamin@web.id', 'Teknik Industri'),
+(7, 'Musaamin', '1234323', 'musaamin@web.id', 'Teknik Industri');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
